2277e593b232269fd64912b3ff933d8e1d2444b5
gerald <geraldbaalham@live.co.uk>
2024-07-25 17:58:24 +0100

    Added first card component 1:09:22

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,15 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard Route</h1>
-    </div>
+    <>
+      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>Total Revenue</CardTitle>
+          </CardHeader>
+        </Card>
+      </div>
+    </>
   );
 }
