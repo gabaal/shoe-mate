17a7ba4ba61a6dc4ce5f0c8319076bf2bed3bcda
gerald <geraldbaalham@live.co.uk>
2024-08-04 14:27:40 +0100

    start on hero section 5:59:17

--- app/components/storefront/Navbar.tsx
+++ app/components/storefront/Navbar.tsx
@@ -1,7 +1,57 @@
-export function Navbar() {
+import Link from "next/link";
+import { NavbarLinks } from "./NavbarLinks";
+import {
+  getKindeServerSession,
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/server";
+import { ShoppingBagIcon } from "lucide-react";
+import { UserDropdown } from "./UserDropdown";
+import { Button } from "@/components/ui/button";
+
+export async function Navbar() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
   return (
-    <div>
-      <h1>Navbar route</h1>
-    </div>
+    <nav className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-5 flex items-center justify-between">
+      <div className="flex items-center">
+        <Link href="/">
+          <h1 className="text-black font-bold text-xl lg:text-3xl">
+            Shoe<span className="text-primary">Mate</span>
+          </h1>
+        </Link>
+        <NavbarLinks />
+      </div>
+
+      <div className="flex items-center">
+        {user ? (
+          <>
+            <Link href="/bag" className="group p-2 flex items-center mr-2">
+              <ShoppingBagIcon className="h-6 w-6 text-gray-500 group-hover:text-gray-600" />
+              <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+                5
+              </span>
+            </Link>
+            <UserDropdown
+              email={user.email as string}
+              name={user.given_name as string}
+              userImage={
+                user.picture ?? `https://avatar.vercel.sh/${user.given_name}`
+              }
+            />
+          </>
+        ) : (
+          <div className="hidden md:flex md:flex-1 md:items-center md:justify-end md:space-x-2">
+            <Button variant="ghost" asChild>
+              <LoginLink>Sign in</LoginLink>
+            </Button>
+            <span className="h-6 w-px bg-gray-200"></span>
+            <Button variant="ghost" asChild>
+              <RegisterLink>Create Account</RegisterLink>
+            </Button>
+          </div>
+        )}
+      </div>
+    </nav>
   );
 }
