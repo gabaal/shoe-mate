fe2ba81e4d5a01aee7f772fd3f9a4fe6fc994e56
gerald <geraldbaalham@live.co.uk>
2024-08-05 12:29:11 +0100

    Hero done

--- app/(storefront)/page.tsx
+++ app/(storefront)/page.tsx
@@ -1,7 +1,9 @@
+import { Hero } from "../components/storefront/Hero";
+
 export default function IndexPage() {
   return (
     <div>
-      <h1>StoreFront page</h1>
+      <Hero />
     </div>
   );
 }
